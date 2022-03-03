a1ed3761ebcbe46cf7eb5e9c73767f6e121293bf
i <iu@example.com>
2022-03-03 14:59:49 +0700

    a

--- src/data.js
+++ src/data.js
@@ -1,29 +1,35 @@
+import image01 from "./img/shop.png"
+import image02 from "./img/movie.png"
+import image03 from "./img/tggd.png"
+import image04 from "./img/weather.png"
+import image05 from "./img/shoope.png"
+
 export const products = [{
         id :1,
-        img : "https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/heroes/social/hoodwink.jpg",
+        img : image01,
         link : "https://laughing-heisenberg-24c87a.netlify.app/"
 
 },
 {
     id :2,
-    img : "https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/heroes/social/hoodwink.jpg",
+    img : image02,
     link : "https://hungry-curie-6fc11d.netlify.app"
 },
 {
     id :3,
-    img : "https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/heroes/social/hoodwink.jpg",
+    img : image03,
     link : "https://comedy-coder.github.io/thanhdat-thegioididong.github.io/"
 
 },
 {
     id :4,
-    img : "https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/heroes/social/hoodwink.jpg",
+    img : image04,
     link : "https://naughty-northcutt-7cb7cd.netlify.app/"
 
 },
 {
     id :5,
-    img : "https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/heroes/social/hoodwink.jpg",
+    img : image05,
     link : "https://comedy-coder.github.io/thanhdat.shop.github.io",
 
     
