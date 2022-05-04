942c3054b3f73bbc64081b893c77586dc3fa34bf
Your Name <you@example.com>
2022-05-04 11:58:23 +0700

    a

--- src/data.js
+++ src/data.js
@@ -1,43 +1,46 @@
 import image01 from "./img/shop.png"
 import image02 from "./img/movie.png"
 import image03 from "./img/tggd.png"
-import image04 from "./img/weather.png"
+import image04 from "./img/queingsystem.png"
 import image05 from "./img/shoope.png"
 import image06 from "./img/tiki.png"
 
-export const products = [{
+export const products = [
+    
+    {
         id :1,
+        img : image04,
+        link : "https://effulgent-capybara-22f61d.netlify.app"
+
+},
+    {
+        id :2,
         img : image01,
         link : "https://laughing-heisenberg-24c87a.netlify.app/"
 
 },
 {
-    id :2,
+    id :3,
     img : image02,
     link : "https://hungry-curie-6fc11d.netlify.app"
 },
 {
-    id :3,
+    id :4,
     img : image03,
     link : "https://comedy-coder.github.io/thanhdat-thegioididong.github.io/"
 
 },
 {
-    id :4,
+    id :5,
     img : image06,
     link : "https://condescending-hamilton-ce27b9.netlify.app"
 },
 {
-    id :5,
+    id :6,
     img : image05,
     link : "https://comedy-coder.github.io/thanhdat.shop.github.io",
 
     
 
 },
-{
-        id :6,
-        img : image04,
-        link : "https://naughty-northcutt-7cb7cd.netlify.app/"
-
-}]
+]
